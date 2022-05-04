32d08a4b7775d6e7720a08b7ee2501b9a725a621
Priyesh Raj Singh <65354440+priyesh-raj-singh@users.noreply.github.com>
2022-05-05 00:43:34 +0530

    Time: 53 ms (28.26%), Space: 27.4 MB (32.30%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
-        int left = -1 , right = -1;
-        int n = nums.size();
+        vector<int> v;
+        v = nums;
+        sort(v.begin() , v.end());
+        int l = -1 , r = -1;
         
-        for(int i = 0 ; i<n-1 ; i++){
-            if(nums[i]>nums[i+1]){
-                left = i ;
+        for(int i = 0 ; i<nums.size() ; i++){
+            if(nums[i]!=v[i]){
+                l = i ;
+                break;
+            }
+                
+        }
+        for(int i = nums.size()-1 ; i>=0 ; i--){
+            if(nums[i]!=v[i]){
+                r = i ;
                 break;
             }
         }
-        for(int i = n-1 ; i>=1 ; i--){
-            if(nums[i-1]>nums[i]){
-                right = i;
-                break;
-            }
-        }
-        if(left==-1) return 0;
-        
-        int mini = nums[left] , maxi = nums[left];
-        for(int i = left ; i<=right ; i++){
-            mini = min(mini , nums[i]);
-            maxi = max(maxi , nums[i]);
-        }
-        left = upper_bound(nums.begin() , nums.begin()+left , mini) - nums.begin();
-        right = lower_bound(nums.begin()+right+1 , nums.end() , maxi) - nums.begin();
-        
-        return right-left;
+        if(l==r)
+            return 0;
+        return r - l +1;
         
     }
 };
